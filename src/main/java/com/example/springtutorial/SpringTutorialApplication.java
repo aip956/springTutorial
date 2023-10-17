42e8c31f21f5c30f83974068d96b39ed4dfcdfc4
Anthea Ip <antheaip964@gmail.com>
2023-10-17 15:44:21 -0400

    Saving

---
+++ src/main/java/com/example/springtutorial/SpringTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.example.springtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialApplication.class, args);
+    System.out.println("In the main java file");
+
+    }
+}
